c4ab33277c5fa0c3dfae684ec738677ef89f2cf0
yerson <yerson_kira@hotmail.com>
2014-04-26 18:43:23 -0700

    first commit

---
+++ TestAsync/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading;
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace TestAsync
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        int number = 0;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Se invoca cuando esta página se va a mostrar en un objeto Frame.
+        /// </summary>
+        /// <param name="e">Datos de evento que describen cómo se llegó a esta página.
+        /// Este parámetro se usa normalmente para configurar la página.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Preparar la página que se va a mostrar aquí.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await thread();
+        }
+        private async Task thread() {
+            while (true) {
+                this.counter.Text = number.ToString();
+                await Task.Delay(1000);
+                this.number++;
+            }
+        }
+    }
+}
